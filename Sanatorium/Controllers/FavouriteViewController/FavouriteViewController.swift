faa1ce8438a2d480efc064f5181925f21d99077b
Makus14 <90136455+Makus14@users.noreply.github.com>
2023-01-18 22:37:08 +0300

    createTabBar

---
+++ Sanatorium/Controllers/FavouriteViewController/FavouriteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteViewController.swift
+//  Sanatorium
+//
+//  Created by Mac on 18/01/2023.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
